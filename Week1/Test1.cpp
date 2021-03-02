6b6f25c284e1a995a4f31ba5e5c15823e2a44b46
hulklee1 <35159494+hulklee1@users.noreply.github.com>
2021-03-02 17:31:53 +0900

    Add files via upload

---
+++ Week1/Test1.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+main()
+{
+	
+/*	printf("Good Morning!.\n");
+//	printf("안녕하세요. Hi");
+	printf("Hello hulklee\n");
+	
+	printf("내 이름은요~ %s 이구요\n\n", "홍길동" );
+	printf("제 나이는요~ %d 입니다.", 9);
+	
+
+	printf("\\a : 경고음 발생"); 
+	printf("%d%d",123,456);  // 123456
+	// 123_____456_____789  출력되도록 하세 요 
+	printf("%8d %8d %8d\n\n\n",123,456,789);  // 123456
+	
+	printf("실수의 출력 : %8.2f %10.3f",3.14159265,456.789123);  // 123456
+*/
+	printf("data type 별 byte 수(int) : %d\n",sizeof(int));
+	 
+	
+	
+	
+}
